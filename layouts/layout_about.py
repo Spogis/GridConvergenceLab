0e3a85272675df554f250632071e6d4b730d517f
spogis <nicolas.spogis@gmail.com>
2024-08-09 20:02:22 -0300

    Update About

--- layouts/layout_about.py
+++ layouts/layout_about.py
@@ -5,7 +5,7 @@ import os
 def layout_about():
     layout = html.Div([
         html.Iframe(id='pdf-viewer', src=os.path.join('assets', 'about.pdf'),
-                    style={'width': '100%', 'height': '600px', 'margin': 'auto',
+                    style={'width': '100%', 'height': '800px', 'margin': 'auto',
                            'text-align': 'center', 'display': 'flex', 'justify-content': 'center'})
     ])
     return layout
